701e340c1b85c4f027ce2e362a808108b1ce52f9
Bogdan Mingboyev <mingboyev.kh@gmail.com>
2020-07-23 10:00:01 +0300

    Launch screen

--- app/src/main/java/space/mera/jobsearch/MainActivity.java
+++ app/src/main/java/space/mera/jobsearch/MainActivity.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Window w=getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    }
 
+        ImageView LogoImageView = (ImageView) findViewById(R.id.main_logo);
+        Animation LogoAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_anim);
+        LogoImageView.startAnimation(LogoAnimation);
+    }
 
 }
